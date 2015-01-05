45f706638667c5909ee25186ce95e2e3c991245e
Ben Conway <bendavidconway@me.com>
2015-01-05 14:00:12 +0000

    Built a second feature test for posting pictures, created a Picture model on the backend, and migrated it's features to ActiveRecord. Second test is not yet passing.

--- spec/features/pictures_feature_spec.rb
+++ spec/features/pictures_feature_spec.rb
@@ -9,3 +9,15 @@ feature 'pictures' do
     end
   end
 end
+
+context 'pictures have been added' do
+  before do
+    Picture.create(name: 'Skydive')
+  end
+
+  scenario 'display picturess' do
+    visit '/pictures'
+    expect(page).to have_content('Skydive')
+    expect(page).not_to have_content('No pictures yet')
+  end
+end
